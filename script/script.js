d1c25ff34715a7dca95bfaf99d464deac004f623
0VitorGabriel <victoropo64@outlook.com>
2023-03-13 19:48:39 -0400

    adicionei evento dom ao input number

--- script/script.js
+++ script/script.js
@@ -12,10 +12,22 @@ porcentagem_gorjeta.addEventListener('input', function () {
     mostrar_porcentagem.innerHTML = Number(porcentagem_gorjeta.value)
 })
 
-porcentagem_gorjeta.addEventListener('mouseup', function () {
-    resultado_gorjeta.innerHTML = Number(porcentagem_gorjeta.value) * Number(valor.value) / 100
+function calcula_gorjeta() {
+    return resultado_gorjeta.innerHTML = Number(porcentagem_gorjeta.value) * Number(valor.value) / 100
+}
 
-    resultado_total.innerHTML = (Number(porcentagem_gorjeta.value) * Number(valor.value) / 100) + Number(valor.value)
+function soma_gorjeta() {
+    return resultado_total.innerHTML = (Number(porcentagem_gorjeta.value) * Number(valor.value) / 100) + Number(valor.value)
+}
+
+porcentagem_gorjeta.addEventListener('mouseup', function () {
+    calcula_gorjeta()
+    soma_gorjeta()
+})
+
+valor.addEventListener('mouseout', function () {
+    calcula_gorjeta()
+    soma_gorjeta()
 })
 
 
