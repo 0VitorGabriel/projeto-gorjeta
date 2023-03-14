288066ffd18dbeccb8202fdee16b1c9262d48f1f
0VitorGabriel <victoropo64@outlook.com>
2023-03-14 09:15:36 -0400

    troquei os eventos DOM e adicionei apenas width

--- script/script.js
+++ script/script.js
@@ -1,6 +1,11 @@
+
+// input:range e input:number
+
 let valor = document.querySelector('input#id_valor')
 
 let porcentagem_gorjeta = document.querySelector('input#id_porcentagem')
+
+/* respectivamente, output que mostra a gorjeta, depois, o output que mostra a gorjeta mais o valor dentro do input:number e o output ao lado do input:range (para mostrar a porcentagem de acordo com o que o usuário desejar). */
 
 let resultado_gorjeta = document.querySelector('output#gorjeta')
 
@@ -12,20 +17,17 @@ porcentagem_gorjeta.addEventListener('input', function () {
     mostrar_porcentagem.innerHTML = Number(porcentagem_gorjeta.value)
 })
 
-function calcula_soma_gorjeta() {
-    let calcula_gorjeta = function () {
-        return resultado_gorjeta.innerHTML = Number(porcentagem_gorjeta.value) * Number(valor.value) / 100
-    }
-    let soma_gorjeta = function () {
-        return resultado_total.innerHTML = (Number(porcentagem_gorjeta.value) * Number(valor.value) / 100) + Number(valor.value)
-    }
-    return calcula_gorjeta() , soma_gorjeta()
+let calcula_gorjeta = () => {
+    return resultado_gorjeta.innerHTML = Number(porcentagem_gorjeta.value) * Number(valor.value) / 100
+}
+let soma_gorjeta = () => {
+    return resultado_total.innerHTML = (Number(porcentagem_gorjeta.value) * Number(valor.value) / 100) + Number(valor.value)
 }
 
-porcentagem_gorjeta.addEventListener('mouseup', function () {
-    calcula_soma_gorjeta()
+porcentagem_gorjeta.addEventListener('mousemove', () => {
+    calcula_gorjeta()
 })
 
-valor.addEventListener('mouseout', function () {
-    calcula_soma_gorjeta()
+valor.addEventListener('mouseout', () => {
+    soma_gorjeta()
 })
